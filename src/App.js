52d79c55c3cc73afd032dc9b8476f37e0d580bf1
RBrunoChV <edu.rz@hotmail.com>
2022-12-11 17:06:25 -0500

    newHomeView

--- src/App.js
+++ src/App.js
@@ -1,112 +1,44 @@
-import { motion } from "framer-motion";
 import { useEffect, useState, useRef } from "react";
 import bg from "./assets/Artboard1.png";
+import styles from "./mainView.module.css";
 
 export default function App() {
-  const _intervalRef = useRef(null);
-  const [startCounter, setStartCounter] = useState(false);
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    if (startCounter) {
-      _intervalRef.current = setInterval(() => {
-        setCounter((counter) => counter + 1);
-      }, 10);
-    } else {
-      clearInterval(_intervalRef.current);
-    }
-    return () => clearInterval(_intervalRef.current);
-  }, [startCounter]);
-
   return (
-    <div>
-      <div style={{height: "100vh", backgroundColor: "#f7f5ed"}}>
+    <div
+      style={{
+        margin: 0,
+        padding: 0,
+        height: "100vh",
+      }}
+    >
+      <header className={styles.headerContainer}>
         <nav>
-          <ul className="d-flex flex-row justify-content-end me-5">
-            <li className="list-unstyled p-2 mx-4">
-              <a className="text-decoration-none" href="#">
-                About
-              </a>
+          <ul className={styles.headerList}>
+            <li className={styles.headerListItem}>
+              <a href="#">About</a>
             </li>
-            <li className="list-unstyled p-2 mx-4">
-              <a className="text-decoration-none" href="#">
-                Projects
-              </a>
+            <li className={styles.headerListItem}>
+              <a href="#">Projects</a>
             </li>
-            <li className="list-unstyled p-2 mx-4">
-              <a className="text-decoration-none" href="#">
-                Contact
-              </a>
+            <li className={styles.headerListItem}>
+              <a href="#">Contact</a>
             </li>
           </ul>
         </nav>
-        <div className="d-flex flex-column align-items-center">
-          <div
-            style={{
-              color: "transparent",
-              fontFamily: "Poppins",
-              letterSpacing: "5px",
-              fontSize: "48px",
-              fontWeight: "800",
-              WebkitTextStroke: "3px #000",
-              lineHeight: "35px",
-            }}
-          >
-            HOLA, YO SOY
-          </div>
-          <div
-            style={{
-              fontFamily: "Syncopate",
-              fontSize: "3.2rem",
-              position: "relative",
-              zIndex: "1"
-            }}
-            onMouseOver={() => {
-              setStartCounter(true);
-            }}
-            onMouseOut={() => {
-              setStartCounter(false);
-            }}
-          >
-            EDUARDO RAMÓN
-            <div
-              style={{
-                position: "absolute",
-                width: "100%",
-                height: "100%",
-                zIndex: "-1",
-                top: "0px",
-                left: "0px",
-              }}
-            >
-              <div
-                style={{
-                  position: "absolute",
-                  backgroundImage:
-                    "url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Location_dot_lightgrey.svg/2000px-Location_dot_lightgrey.svg.png)",
-                  backgroundRepeat: "repeat-x",
-                  backgroundPositionX: counter,
-                  backgroundSize: "5%",
-                  width: "100%",
-                  height: "100%",
-                  top: "45%",
-                }}
-              ></div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.home}>
+          <div className={styles.homeTitle}>
+            <div className={styles.homeTitleTop}>Hello, I am</div>
+            <div className={styles.homeTitleBottom}>EDUARDO RAMÓN</div>
+            <div className={styles.homeSubtitle}>I am Just a Coder</div>
+            <div className={styles.homeButton}>
+              <a href="#">view more</a>
             </div>
           </div>
-          <div style={{}}>Just a Coder</div>
-        </div>
-        <div
-          className="ps-3"
-          style={{
-            fontFamily: "poppins",
-            fontSize: "180px",
-            fontWeight: "800",
-          }}
-        >
-          CODER
         </div>
       </div>
+      <footer className={styles.coderText}>CODER</footer>
     </div>
   );
 }
