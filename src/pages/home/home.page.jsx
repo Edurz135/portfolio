4de3cb24c3557287479afc3362640063871c3969
Edurz135 <edu.rz@hotmail.com>
2024-01-11 22:18:58 -0500

    a

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -4,10 +4,86 @@ export default function HomePage() {
   useEffect(() => {}, []);
 
   return (
-    <div className="p-0 m-0">
-        <div className="font-fdecorated text-9xl w-full h-full">
-            1956
+    <div className="pt-10 px-10 m-0 bg-color-two">
+      <div className="flex justify-between">
+        <span className="font-two font-bold text-5xl">EDURZ</span>
+        <span className="font-two font-bold text-5xl">+</span>
+      </div>
+      <div className="flex flex-row">
+        <span>
+          <p className="font-two font-bold">Name-</p>
+          <p className="font-two">Eduardo</p>
+        </span>
+        <span>
+          <p className="font-two font-bold">Lastname-</p>
+          <p className="font-two">Ramón</p>
+        </span>
+        <span>
+          <p className="font-two font-bold">Role-</p>
+          <p className="font-two">Coder</p>
+        </span>
+        <span>
+          <p className="font-two font-bold">Birth-</p>
+          <p className="font-two">22.07.2003</p>
+        </span>
+      </div>
+
+      <br />
+
+      <div className="flex justify-between text-5xl font-bold font-two">
+        <span>
+          <span className="pr-2">&gt;&gt;</span>
+          <span className="underline">ABOUT ME</span>
+        </span>
+        <span>
+          <span className="pr-2">&gt;&gt;</span>
+          <span className="underline">PROJECTS</span>
+        </span>
+        <span>
+          <span className="pr-2">&gt;&gt;</span>
+          <span className="underline">CONTACT</span>
+        </span>
+      </div>
+
+      <br />
+
+      <div className="flex justify-between items-end h-28 px-5 bg-color-one font-one text-color-two font-bold rounded-3xl">
+        <span className="text-8xl">PORTFOLIO</span>
+        <span className="text-4xl pb-3">It's me</span>
+      </div>
+
+      <br />
+
+      <div className="flex">
+        <div className="flex-1 h-[40rem] bg-color-three rounded-3xl"></div>
+        <div className="flex-1">
+          <div className="font-one text-color-one font-bold text-[11rem] text-right">
+            RAMÓN
+          </div>
+          <div className="font-two text-color-three text-justify text-xl pl-10 whitespace-pre-line">
+            Student of the <b>Systems Engineering</b> degree at the University of Lima.
+            I have experience in web page development and of 2D and 3D video
+            games under the Unity platform. Am a <b>responsible</b>, organized person,
+            with great ease of learning and committed to work.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et <b>dolore</b> magna aliqua. Ut enim
+            ad minim veniam.
+            <br />
+            <br />
+            Student of the Systems Engineering degree at the University of Lima.
+            I have experience in web page development and of 2D and 3D video
+            games under the Unity platform. Am a responsible, organized person,
+            with great ease of learning and committed to work.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </div>
         </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontSize: {
         "12xl": ["11.4rem", "8rem"],
       },
       colors: {
-        "con-primary": "#f9e9d1",
+        "color-one": "#DB9C3E",
+        "color-two": "#EFF0E3",
+        "color-three": "#20140D",
       },
       fontFamily: {
-        fheader: ["Libre Bodoni"],
-        ftext: ["Montserrat Alternates"],
-        fdecorated: ["Niagara Solid"],
+        "one": ["Libre Bodoni"],
+        "two": ["Montserrat Alternates"],
+        "three": ["Niagara Solid"],
       },
     },
   },
